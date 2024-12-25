7cd854caa0710ba74ee986bf65f49ab8ac76f9b6
Mari0 <discordtako8@gmail.com>
2024-12-25 14:52:03 +0100

    first commit

---
+++ menu.js
@@ -0,0 +1,80 @@
+// hambuerger menu
+
+var menu = document.getElementById("menu-toggle");
+var menuItems = document.getElementById("menu");
+
+var menuStatus = 0;
+menu.addEventListener("click", ()=> {
+    menuItems.classList.toggle("active");
+    if (menuStatus == 0) {
+        menu.style = "transform: rotate(90deg)";
+        menuStatus = 1;
+    } else if (menuStatus == 1) {
+        menu.style = "transform: rotate(0deg)";
+        menuStatus = 0;
+    }
+});
+
+window.addEventListener("click", event=> {
+    if (event.target === menuItems) {
+        menu.style = "transform: rotate(0deg);";
+        menuStatus = 0;
+        menuItems.classList.remove("active");
+    }
+});
+
+
+
+
+// Navigation (HEADER)
+var weapons = document.querySelectorAll(".weapons"); 
+var activeMenu = null;
+var activeWeapon = null; 
+
+weapons.forEach((element, index) => {
+    var weaponsMenu = document.querySelectorAll(".weaponsMenu"); 
+    if (weaponsMenu[index]) {
+        element.addEventListener("click", () => {
+            if (activeMenu !== null && activeMenu !== weaponsMenu[index]) {
+                activeMenu.classList.remove("activeWeapons"); 
+            }
+            if (activeWeapon !== null && activeWeapon !== element) {
+                activeWeapon.style.borderBottom = "";
+            }
+            if (weaponsMenu[index].classList.contains("activeWeapons")) {
+                weaponsMenu[index].classList.remove("activeWeapons");
+                activeMenu = null;
+                activeWeapon = null;
+            } else {
+                weaponsMenu[index].classList.add("activeWeapons");
+                activeMenu = weaponsMenu[index];
+                element.style.borderBottom = "solid 2px white";
+                activeWeapon = element; 
+            }
+        });
+
+        weaponsMenu[index].querySelectorAll('.item').forEach((item) => {
+            item.addEventListener('click', () => {
+                weaponsMenu.forEach(menu => menu.classList.remove("activeWeapons"));
+                activeMenu = null;
+                if (activeWeapon) {
+                    activeWeapon.style.borderBottom = "";
+                }
+                activeWeapon = null;
+                // Hide the menu in responsive version
+                menuItems.classList.remove("active");
+                menu.style = "transform: rotate(0deg)";
+                menuStatus = 0;
+            });
+        });
+    }
+});
+
+//scroll
+var scroll = document.querySelectorAll(".weaponsMenu");
+scroll.forEach(menu => {
+    menu.addEventListener("wheel", (event) => { 
+        event.preventDefault();
+        menu.scrollLeft += event.deltaY;
+    });
+});
---
+++ server.php
@@ -0,0 +1,26 @@
+<?php
+    $conn = new mysqli("localhost", "root", "", "csgo");
+    if ($conn -> connect_error) {
+        die("Connection failed: " . $conn -> connect_error);
+    }
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['category'])) {
+        $category = $conn -> real_escape_string($_POST['category']);
+        $sql = "SELECT *
+                FROM `$category`";
+
+        if ($result = $conn -> query($sql)) {
+            if($result -> num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<div class='box-item'>
+                            <p>{$row['skin_name']}</p>
+                            <img src='./item_img/{$_POST['category']}/{$row['skin_name']}.png' alt='{$row['skin_name']}'>
+                        </div>";   
+                }
+            } else {
+                echo "0 results";
+            }
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . $conn -> error;
+        }
+    }
+?>
